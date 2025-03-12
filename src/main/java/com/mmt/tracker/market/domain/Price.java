e35206f4b3c34c59055e4ca79d8023750dfacceb
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-03-12 09:46:33 +0900

    refactor(Price): - @Notnull 어노테이션 제거 후 @Column의 nullable = false로 교체 - 미사용 주석 제거

--- src/main/java/com/mmt/tracker/market/domain/Price.java
+++ src/main/java/com/mmt/tracker/market/domain/Price.java
@@ -5,62 +5,46 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Entity
-//@Table(
-//        name = "prices",
-//        uniqueConstraints =
-//                @UniqueConstraint(
-//                        columnNames = {
-//                            "item_name",
-//                            "stat_type",
-//                            "starForce",
-//                            "stat_percent",
-//                            "date"
-//                        }),
-//        indexes = @Index(name = "idx_item_percent", columnList = "item_name, stat_percent"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class Price {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "CHAR(40)")
-    @NonNull
+    @Column(columnDefinition = "CHAR(40)", nullable = false)
     private String itemName;
 
-    @Column(columnDefinition = "CHAR(10)")
-    @NonNull
+    @Column(columnDefinition = "CHAR(10)", nullable = false)
     private String statType;
 
-    @Column(columnDefinition = "SMALLINT")
-    @NonNull
+    @Column(columnDefinition = "SMALLINT", nullable = false)
     private Short starForce;
 
-    @Column(columnDefinition = "SMALLINT")
-    @NonNull
+    @Column(columnDefinition = "SMALLINT", nullable = false)
     private Short statPercent;
 
-    @NonNull
+    @Column(nullable = false)
     private Long amount;
 
-    @Column(name = "date")
-    @NonNull
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
     private LocalDateTime date;
 
-    public Price(String itemName, String statType, short starforce, short statPercent, long amount, LocalDateTime date) {
+    public Price(
+            String itemName, String statType, short starForce, short statPercent, long amount, LocalDateTime date
+    ) {
         this.itemName = itemName;
         this.statType = statType;
-        this.starForce = starforce;
+        this.starForce = starForce;
         this.statPercent = statPercent;
         this.amount = amount;
         this.date = date;
