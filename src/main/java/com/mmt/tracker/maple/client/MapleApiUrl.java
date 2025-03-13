c7ecb5d362c2716dd72577ac0ef6716c1c69ceb9
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-03-13 19:09:15 +0900

    refactor(MapleUri): MapleApiClient에 존재하는 URL 상수 Enum으로 분리

--- src/main/java/com/mmt/tracker/maple/client/MapleApiUrl.java
+++ src/main/java/com/mmt/tracker/maple/client/MapleApiUrl.java
@@ -1,9 +1,10 @@
 package com.mmt.tracker.maple.client;
 
 public enum MapleApiUrl {
+
     BASE_URL("https://open.api.nexon.com/maplestory/v1"),
     GET_CHARACTER_OCID_BY_NAME("/id?character_name=%s"),
-    GET_CHARACTER_EQUIPMENT_BY_OCID("/character/item-equipment?ocid=%s"),
+    GET_CHARACTER_ITEM_EQUIPMENT_BY_OCID("/character/item-equipment?ocid=%s"),
     ;
 
     private final String url;
